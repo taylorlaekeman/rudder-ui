35aa3d420bc9d83f5ba226c192fb659e02d4ab1f
taylor <taylor.laekeman@gmail.com>
2020-07-06 22:40:55 -0400

    add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 
 import 'App.css';
 import logo from 'logo.svg';
 import store from 'state';
+import theme from 'styles/theme';
 
 function App() {
   return (
     <ReduxProvider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {'Edit '}
-            <code>src/App.tsx</code>
-            {' and save to reload.'}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              {'Edit '}
+              <code>src/App.tsx</code>
+              {' and save to reload.'}
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </ThemeProvider>
     </ReduxProvider>
   );
 }
