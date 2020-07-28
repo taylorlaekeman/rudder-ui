d3f06230f60a7511007daf5a06eaf5fc7f773998
taylor <taylor.laekeman@gmail.com>
2020-07-27 22:31:54 -0400

    shrink medium text and left align all text

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -5,16 +5,19 @@ const font = {
     font-family: 'Raleway', sans-serif;
     font-size: 0.8rem;
     font-weight: 400;
+    text-align: left;
   `,
   medium: css`
     font-family: 'Raleway', sans-serif;
-    font-size: 1.2rem;
+    font-size: 1rem;
     font-weight: 400;
+    text-align: left;
   `,
   large: css`
     font-family: 'Raleway', sans-serif;
     font-size: 1.2rem;
     font-weight: 400;
+    text-align: left;
   `,
 };
 
