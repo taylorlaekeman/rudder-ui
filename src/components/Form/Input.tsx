70edad97ba9a5c1a5f4d6d76ed6fa005fd9e494c
taylor <taylor.laekeman@gmail.com>
2020-08-08 16:15:12 -0400

    add isStruck, onKeyDown, and placeholder props to input component

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -3,39 +3,51 @@ import styled from 'styled-components';
 
 import noop from 'utils/noop';
 
-const StyledInput = styled.input<{ area: string }>`
-  background: none;
-  border: none;
-  border-bottom: solid ${({ theme }) => theme.colours.border} 1px;
-  color: ${({ theme }) => theme.colours.text};
-  grid-area: ${({ area }) => area};
-  outline: none;
-  width: 100%;
-  ${({ theme }) => theme.font.medium};
-`;
-
 type propTypes = {
   area?: string;
+  isStruck?: boolean;
   onChange?: { (value: string): void };
+  onKeyDown?: { (value: number): void };
+  placeholder?: string;
   value?: string;
 };
 
 const Input: React.FunctionComponent<propTypes> = ({
   area = '',
+  isStruck = false,
   onChange = noop,
+  onKeyDown = noop,
+  placeholder = '',
   value = '',
 }: propTypes) => (
   <StyledInput
     area={area}
+    $isStruck={isStruck}
     onChange={(event) => onChange(event.target.value)}
+    onKeyDown={(event) => onKeyDown(event.keyCode)}
+    placeholder={placeholder}
     type="text"
     value={value}
   />
 );
 
-Input.defaultProps = {
-  onChange: noop,
-  value: '',
-};
+const StyledInput = styled.input<{ area: string; $isStruck: boolean }>`
+  background: none;
+  border: none;
+  color: ${({ theme }) => theme.colours.text};
+  grid-area: ${({ area }) => area};
+  outline: none;
+  width: 100%;
+  ${({ $isStruck }) => $isStruck && 'text-decoration: line-through;'}
+  ${({ theme }) => theme.font.medium};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colours.text};
+    font-style: italic;
+    opacity: 1;
+  }
+`;
+
+export const BACKSPACE = 8;
 
 export default Input;
