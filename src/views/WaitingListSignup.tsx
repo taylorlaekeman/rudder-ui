2eac5e3d760f9708c2b3c3f851361e2b9a035475
taylor <taylor.laekeman@gmail.com>
2020-08-20 21:10:41 -0400

    add success state to waiting list page

--- src/views/WaitingListSignup.tsx
+++ src/views/WaitingListSignup.tsx
@@ -6,27 +6,37 @@ import { mutations } from 'api';
 import Button from 'components/Button';
 import UnstyledForm from 'components/Form';
 import Input from 'components/Form/Input';
+import Checkmark from 'components/logos/Checkmark';
 
 const WaitingListSignup: FunctionComponent = () => {
   const [email, setEmail] = useState('');
   const [
     registerEmail,
-    { error, loading: isLoading },
+    { data, error, loading: isLoading },
   ] = useMutation(mutations.registerEmail, { errorPolicy: 'all' });
+
   return (
     <Form onSubmit={() => registerEmail({ variables: { email } })}>
-      <Input
-        area="input"
-        hasError={!!error?.message}
-        label="email"
-        onChange={setEmail}
-        placeholder="Enter your email"
-        type="email"
-        value={email}
-      />
-      <Button area="button" isLoading={isLoading} type="submit">
-        Join the waiting list
-      </Button>
+      {data ? (
+        <Success>Thanks for joining the waiting list!</Success>
+      ) : (
+        <Input
+          area="input"
+          hasError={!!error?.message}
+          label="email"
+          onChange={setEmail}
+          placeholder="Enter your email"
+          type="email"
+          value={email}
+        />
+      )}
+      {data ? (
+        <Checkmark />
+      ) : (
+        <Button area="button" isLoading={isLoading} type="submit">
+          Join the waiting list
+        </Button>
+      )}
     </Form>
   );
 };
@@ -38,6 +48,12 @@ const Form = styled(UnstyledForm)`
     'button .    ';
   grid-template-columns: max-content 1fr;
   margin-top: 256px;
+`;
+
+const Success = styled.p`
+  display: inline-block;
+  grid-area: input;
+  margin-bottom: 64px;
 `;
 
 export default WaitingListSignup;
