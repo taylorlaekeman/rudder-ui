0db850abfa50bee77c7b981ff129ce8e6834dd26
taylor <taylor.laekeman@gmail.com>
2020-09-08 19:54:44 -0400

    move login and loading components into rudder component

--- src/views/Rudder.tsx
+++ src/views/Rudder.tsx
@@ -2,16 +2,39 @@ import React, { FunctionComponent, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
 
+import LoadingIndicator from 'components/LoadingIndicator';
 import analytics from 'utils/analytics';
 import Goals from 'views/Goals';
 import Header from 'views/Header';
+import Login from 'views/Login';
 import SprintCreator from 'views/SprintCreator';
 import Sprints from 'views/Sprints';
 
-const Rudder: FunctionComponent = () => {
+const Rudder: FunctionComponent<propTypes> = ({
+  isAuthenticated = false,
+  token = '',
+}: propTypes) => {
   useEffect(() => {
     analytics.logVisit();
   }, []);
+
+  if (!isAuthenticated)
+    return (
+      <Wrapper>
+        <Main>
+          <Login />
+        </Main>
+      </Wrapper>
+    );
+
+  if (!token)
+    return (
+      <Wrapper>
+        <Main>
+          <LoadingIndicator />
+        </Main>
+      </Wrapper>
+    );
 
   return (
     <Wrapper>
@@ -33,10 +56,18 @@ const Rudder: FunctionComponent = () => {
   );
 };
 
+type propTypes = {
+  isAuthenticated?: boolean;
+  token?: string;
+};
+
 const Wrapper = styled.article`
   align-items: center;
+  background-color: green;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  width: 100%;
 `;
 
 const Main = styled.main`
